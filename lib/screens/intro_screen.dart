a8c33551e60aa6fba469d44b6e72c8c0030beec4
Andre Kho <andreyonathan65@gmail.com>
2021-03-15 00:28:41 +0700

    Add first test

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'package:simple_animations/simple_animations.dart';
-import 'package:supercharged/supercharged.dart';
+// import 'package:simple_animations/simple_animations.dart';
+// import 'package:supercharged/supercharged.dart';
 
 class IntroScreen extends StatelessWidget {
   @override
@@ -31,26 +31,26 @@ class IntroScreen extends StatelessWidget {
             ),
           ),
         ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            child: AnimationDeveloperTools(
-              position: AnimationDeveloperToolsPosition.bottom,
-              child: PlayAnimation<Color?>(
-                tween: Colors.red.tweenTo(Colors.blue).curved(Curves.easeInOut),
-                duration: 5.seconds,
-                developerMode: true,
-                builder: (context, child, value) {
-                  return Container(
-                    color: value,
-                    width: 100,
-                    height: 100,
-                  );
-                },
-              ),
-            ),
-          ),
-        ),
+        // Expanded(
+        //   flex: 1,
+        //   child: Container(
+        //     child: AnimationDeveloperTools(
+        //       position: AnimationDeveloperToolsPosition.bottom,
+        //       child: PlayAnimation<Color?>(
+        //         tween: Colors.red.tweenTo(Colors.blue).curved(Curves.easeInOut),
+        //         duration: 5.seconds,
+        //         developerMode: true,
+        //         builder: (context, child, value) {
+        //           return Container(
+        //             color: value,
+        //             width: 100,
+        //             height: 100,
+        //           );
+        //         },
+        //       ),
+        //     ),
+        //   ),
+        // ),
       ],
     );
   }
