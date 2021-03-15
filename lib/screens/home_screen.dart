133eeacb9f451e97b4baf70e1ad4f0dc6306d276
Andre Kho <andreyonathan65@gmail.com>
2021-03-15 22:44:16 +0700

    Add plasma background

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 import 'about_screen.dart';
 import 'contact_screen.dart';
@@ -11,13 +12,50 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView(
-        scrollDirection: Axis.horizontal,
+      body: Stack(
         children: [
-          IntroScreen(),
-          PortfolioScreen(),
-          AboutScreen(),
-          ContactScreen(),
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                tileMode: TileMode.mirror,
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xff021b71),
+                  Color(0xff0538ab),
+                ],
+                stops: [
+                  0,
+                  1,
+                ],
+              ),
+              backgroundBlendMode: BlendMode.srcOver,
+            ),
+            child: PlasmaRenderer(
+              type: PlasmaType.infinity,
+              particles: 20,
+              color: Color(0xaf1c2cbd),
+              blur: 0.5,
+              size: 0.5830834600660535,
+              speed: 3.916667302449544,
+              offset: 0,
+              blendMode: BlendMode.plus,
+              particleType: ParticleType.atlas,
+              variation1: 0,
+              variation2: 0,
+              variation3: 0,
+              rotation: 0,
+            ),
+          ),
+          PageView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              IntroScreen(),
+              PortfolioScreen(),
+              AboutScreen(),
+              ContactScreen(),
+            ],
+          ),
         ],
       ),
     );
--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_animations/simple_animations.dart';
 
 // import 'package:simple_animations/simple_animations.dart';
 // import 'package:supercharged/supercharged.dart';
@@ -6,52 +7,23 @@ import 'package:flutter/material.dart';
 class IntroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 5,
-          child: Container(
-            child: Center(
-              child: Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(text: 'Hi 👋 I\'m '),
-                    TextSpan(
-                      text: 'Andre Kho',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
-                  ],
-                  style: TextStyle(
-                    fontSize: 64,
-                  ),
+    return Container(
+      child: Center(
+        child: Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(text: 'Hi 👋 I\'m '),
+              TextSpan(
+                text: 'Andre Kho',
+                style: TextStyle(
+                  fontWeight: FontWeight.w800,
                 ),
               ),
-            ),
+            ],
+            style: TextStyle(fontSize: 64, color: Colors.white),
           ),
         ),
-        // Expanded(
-        //   flex: 1,
-        //   child: Container(
-        //     child: AnimationDeveloperTools(
-        //       position: AnimationDeveloperToolsPosition.bottom,
-        //       child: PlayAnimation<Color?>(
-        //         tween: Colors.red.tweenTo(Colors.blue).curved(Curves.easeInOut),
-        //         duration: 5.seconds,
-        //         developerMode: true,
-        //         builder: (context, child, value) {
-        //           return Container(
-        //             color: value,
-        //             width: 100,
-        //             height: 100,
-        //           );
-        //         },
-        //       ),
-        //     ),
-        //   ),
-        // ),
-      ],
+      ),
     );
   }
 }
